b189bccf3ca8d7ce5640398c469434d65a38e7f1
mgoddard <mgoddard@cockroachlabs.com>
2020-08-07 15:47:14 -0400

    Pulling the SQL into its own file

---
+++ crdb_text_search.sql
@@ -0,0 +1,40 @@
+-- DDL for the two tables
+DROP TABLE IF EXISTS docs;
+CREATE TABLE docs
+(
+  idx_name TEXT
+  , uri TEXT
+  , content TEXT[]
+  , n_words INT
+  , PRIMARY KEY (idx_name, uri)
+);
+
+DROP TABLE IF EXISTS words;
+CREATE TABLE words
+(
+  idx_name TEXT
+  , uri TEXT
+  , word TEXT
+  , cnt INT
+);
+
+-- Secondary indexes
+CREATE INDEX ON docs USING GIN(content);
+CREATE INDEX ON words (idx_name, word) STORING (cnt);
+
+-- Search query
+WITH d AS (
+  SELECT idx_name, uri, n_words
+  FROM docs
+  WHERE content @> '{crdb_docs, follow, read}'
+), w AS (
+  SELECT idx_name, uri, SUM(cnt) n
+  FROM words
+  WHERE idx_name = 'crdb_docs' AND word IN ('follow', 'read')
+  GROUP BY (idx_name, uri)
+)
+SELECT w.uri, (100.0 * n/n_words)::NUMERIC(9, 3) score FROM w
+JOIN d ON d.idx_name = w.idx_name AND d.uri = w.uri
+ORDER BY score DESC
+LIMIT 12;
+
--- html_indexer.py
+++ html_indexer.py
@@ -19,49 +19,6 @@ import time
 # ./html_indexer.py crdb_docs *.html  36.17s user 1.25s system 42% cpu 1:27.18 total
 #
 
-"""
--- Search query
-WITH d AS (
-  SELECT idx_name, uri, n_words
-  FROM docs
-  WHERE content @> '{crdb_docs, follow, read}'
-), w AS (
-  SELECT idx_name, uri, SUM(cnt) n
-  FROM words
-  WHERE idx_name = 'crdb_docs' AND word IN ('follow', 'read')
-  GROUP BY (idx_name, uri)
-)
-SELECT w.uri, (100.0 * n/n_words)::NUMERIC(9, 3) score FROM w
-JOIN d ON d.idx_name = w.idx_name AND d.uri = w.uri
-ORDER BY score DESC
-LIMIT 12;
-
--- DDL for the two tables
-DROP TABLE IF EXISTS docs;
-CREATE TABLE docs
-(
-  idx_name TEXT
-  , uri TEXT
-  , content TEXT[]
-  , n_words INT
-  , PRIMARY KEY (idx_name, uri)
-);
-
-DROP TABLE IF EXISTS words;
-CREATE TABLE words
-(
-  idx_name TEXT
-  , uri TEXT
-  , word TEXT
-  , cnt INT
-);
-
--- Secondary indexes
-CREATE INDEX ON docs USING GIN(content);
-CREATE INDEX ON words (idx_name, word) STORING (cnt);
-
-"""
-
 docs_sql = "INSERT INTO docs (idx_name, uri, content, n_words) VALUES "
 words_sql = "INSERT INTO words (idx_name, uri, word, cnt) VALUES "
 

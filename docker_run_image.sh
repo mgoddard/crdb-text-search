9c9c19f132627b1b17bb7517ab696415df17eaac
mgoddard <mgoddard@cockroachlabs.com>
2020-09-22 10:04:24 -0400

    Ignoring the untar'd data directory

--- docker_run_image.sh
+++ docker_run_image.sh
@@ -3,5 +3,5 @@
 . ./current_tag.sh
 
 # Change these environment variables as required to suit your deployment
-docker run --rm -p 18080:18080 -e PGHOST=192.168.1.3,PGDATABASE=defaultdb,PGUSER=root,PGPORT=5432,FLASK_PORT=18080 mgoddard/crdb-search-app:$tag
+docker run --rm -p 18080:18080 -e PGHOST=192.168.1.8,PGDATABASE=defaultdb,PGUSER=root,PGPORT=5432,FLASK_PORT=18080 mgoddard/crdb-search-app:$tag
 
